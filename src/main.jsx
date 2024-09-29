1f00d82760613db8abfeeff2a1ba8a7a02e5d737
elena <e.berdnik@amio.ru>
2024-09-29 17:13:41 +0300

    hw-3

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import { createRoot } from "react-dom/client";
-import { restaurants } from "../materials/mock.js";
 import App from "./components/app/App";
 
 const root = createRoot(document.getElementById("root"));
