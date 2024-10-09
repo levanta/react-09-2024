0265436a16c1e3f07cee57264b770c7ed0e835a1
elena <e.berdnik@amio.ru>
2024-10-09 04:35:53 +0300

    hw-6

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import Layout from "./Layout";
 import Restaurants from "../restaurants/Restaurants";
 import { restaurants } from "../../../materials/mock.js";
+import UserProvider from "../../contexts/user-context/user-context";
+import ThemeProvider from "../../contexts/theme-context/theme-context.jsx";
 import "../../app.css";
 
 const App = () => {
   return (
-    <Layout>
-      <Restaurants restaurants={restaurants} />
-    </Layout>
+    <ThemeProvider>
+      <UserProvider>
+        <Layout>
+          <Restaurants restaurants={restaurants} />
+        </Layout>
+      </UserProvider>
+    </ThemeProvider>
   );
 };
 
