e1ac88caf69177ce2b5ce7b043646bea81e4605b
elena <e.berdnik@amio.ru>
2024-10-09 03:20:20 +0300

    hw-5

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Layout from "./Layout";
 import Restaurants from "../restaurants/Restaurants";
 import { restaurants } from "../../../materials/mock.js";
+import "../../app.css";
 
 const App = () => {
   return (
